a03358a29c0dae0eb224242b088f797c8100689e
varunteja_542 <parshavenivarunteja@gmail.com>
2025-07-28 11:28:18 +0530

    1)

--- LoggingMiddleware/log.js
+++ LoggingMiddleware/log.js
@@ -1,1 +1,13 @@
 
+const handler = (req, res, next) => {
+  const start = Date.now();
+
+  res.on('finish', () => {
+    const duration = Date.now() - start;
+    console.log(`[${req.method}] ${req.originalUrl} - ${res.statusCode} - ${duration}ms`);
+  });
+
+  next(); 
+};
+
+module.exports = logger;
